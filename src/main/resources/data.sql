d501b2a3730dc7edffab2e36f331044cd5b8430e
anadantasp <caroldantasp@gmail.com>
2024-04-18 15:51:29 -0300

    deleção e cadastro de tarefa e views relacionadas

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TASK (title, description, score, status) VALUES ('Teste Unitários', 'Realizar os testes das classes', 50, 0 );
+INSERT INTO TASK (title, description, score, status) VALUES ('Prototipo Figma', 'Criar o prototipo das telas', 20, 10);
+INSERT INTO TASK (title, description, score, status) VALUES ('Modelagem do BD', 'Criar os diagramas de relacionamento', 70, 50);
